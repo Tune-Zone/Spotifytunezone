c0800c5d9320d733b7b532bf264d2661c4c6443c
harish <harishkumarvijayarajan@gmail.com>
2021-11-23 01:50:48 +0530

    port problem

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const Tune = require('./Model/Tunepost')
 
 const app = express()
-const port = 8000
+const port = 3000
 
 app.use(cors()) // To handle cross-origin requests
 app.use(express.json()); // To parse JSON bodies
